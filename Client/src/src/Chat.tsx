39ec2df07fce0dbbb5b1b42206ea8574b5b87e92
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-04-23 23:55:19 +0530

    #

--- Client/src/src/Chat.tsx
+++ Client/src/src/Chat.tsx
@@ -144,7 +144,7 @@ function Chat() {
   };
 
   return (
-    <Card className="rounded-xl border w-full max-w-2xl mx-auto mt-4 sm:mt-2">
+    <Card className="h-[98vh] overflow-y-auto rounded-xl border w-full max-w-2xl mx-auto mt-2 sm:mt-2">
       {/* Top-right corner toggle */}
       <div className="absolute top-5 sm:top-4 right-4 z-10">
         <ModeToggle />
